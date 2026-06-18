7df77bf101aa5ab5107b99352cccac18065aa8d3
Ilya Rubezhanskiy <ilia.rubezhanskii@indusoft.ru>
2026-06-18 14:56:06 +0300

    Сделана функция замены ключей объекта значениями и наоборот

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,19 @@
  * @returns {object | undefined} - returns the new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    try
+    {
+    for(let [key, value] of Object.entries(obj))
+    {
+        if(value != undefined || value != null)
+        {
+            delete obj[`${key}`];
+            obj[`${value}`] = key;
+        } else throw new Error();
+    }
+    } catch
+    {
+        return undefined;
+    }
+    return obj;
 }
