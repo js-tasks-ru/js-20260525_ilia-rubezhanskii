c5ee2a66fbcfcafec3b9e2548fa0c6b7741447c7
Ilya Rubezhanskiy <ilia.rubezhanskii@indusoft.ru>
2026-06-18 14:20:14 +0300

    Сделана функция возврата новой функции, возвращающей значение по указанному пути и объекту

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,34 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    let [...arr] = path.split('.');
+    /**
+     * 
+     * @param {Object} obj 
+     */
+    let newFunction = function(obj)
+    {
+        try
+        {
+            let objArr = [obj];
+            let result;
+            for(let i = 0; i < arr.length; i++)
+            {
+                let prop = (objArr[i])[`${Object.keys(objArr[i]).find(x => x.toString() === arr[i])}`];
+                if(prop != null && Object.getPrototypeOf(prop) === Object.prototype)
+                {
+                    objArr[i+1] = prop;
+                } else
+                {
+                    result = prop;
+                }
+            }
+            return result;
+        } catch
+        {
+            console.log("Ошибка распознания пути объекта");
+            return undefined;
+        }
+    }
+    return newFunction;
 }
